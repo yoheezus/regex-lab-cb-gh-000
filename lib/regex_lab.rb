a769922e0b82f4c0d3165af770dc9fc3fb2fa3b2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-11 20:48:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,32 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/\A[aeiouAEIOU]+[a-zA-Z]*/)
+        true
+    else
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+[a-z]+ing/)
 end
 
 def words_five_letters_long(text)
+    text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/\b[A-Z]\S*[!.,?:;]/)
+        true
+    else
+        false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    if phone.match(/(\d{10}|\(\d{3}\)\d{3}(-\d{4}|\d{4})|\d{3}\s\d{3}\s\d{4})\b/)
+        true
+    else
+        false
+    end
 end
